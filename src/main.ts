a876a17d2750fa34b5ab37bf1cc4143932e44c23
wu649782027 <15151851516@163.com>
2024-01-03 18:46:50 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: "http://127.0.0.1:5173",
+    credentials: true,
+    // all headers that client is allowed to use
+    allowedHeaders: [
+      "Accept",
+      "Authorization",
+      "Content-Type",
+      "X-Requested-With",
+      "apollo-require-preflight"
+    ],
+    methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"]
+  });
   await app.listen(3000);
 }
+
 bootstrap();
